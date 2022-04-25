c961b4d5f284a569a02bd32b2e0c88f107413aca
Omnish Kundariya <omnish.inexture@gmail.com>
2022-04-26 01:44:50 +0530

    SB

--- front-end/src/components/Admin/sidebar/SideBarHome.js
+++ front-end/src/components/Admin/sidebar/SideBarHome.js
@@ -1,7 +1,11 @@
 import React from "react";
 
 const SideBarHome = () => {
-    return <div className="adminPageSideBarHomePage">SideBarHome</div>;
+    return (
+        <div className="adminPageSideBarHomePage">
+            <h1>Welcome Admin</h1>
+        </div>
+    );
 };
 
 export default SideBarHome;
